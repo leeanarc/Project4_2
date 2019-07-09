45b02245879bdad7df273fcbf756480f93db64c4
leean_arc <tywinlee1017@naver.com>
2019-07-09 10:26:56 +0900

    7/9 수정

--- app/src/main/java/com/cookandroid/project4_2/MainActivity.java
+++ app/src/main/java/com/cookandroid/project4_2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cookandroid.project4_2;
 
 import androidx.annotation.IdRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     /*Button btnOK;*/
     ImageView imgPet;
     Switch switch1;
-
+    Button exitButton, clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setTitle("애완동물 사진 보기");
 
         /*text1= (TextView) findViewById(R.id.Text1);*/
-        final Switch sw = (Switch)findViewById(R.id.Switch1);
-        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+       final Switch sw = (Switch)findViewById(R.id.Switch1);
+       /* chkAgree = (CheckBox) findViewById(R.id.ChkAgree);*/
 
         text2= (TextView) findViewById(R.id.Text2);
         rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
@@ -46,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         /*btnOK = (Button) findViewById(R.id.BtnOK);*/
         imgPet = (ImageView) findViewById(R.id.ImgPet);
-        Button exitButton = (Button) findViewById(R.id.exitButton);
-
+        exitButton = (Button) findViewById(R.id.exitButton);
+        clear = (Button) findViewById(R.id.btn_clear);
 
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -57,11 +59,16 @@ public class MainActivity extends AppCompatActivity {
                     rGroup1.setVisibility(View.VISIBLE);
                     /*btnOK.setVisibility(View.VISIBLE);*/
                     imgPet.setVisibility(View.VISIBLE);
+                    exitButton.setVisibility(View.VISIBLE);
+                    clear.setVisibility(View.VISIBLE);
                 } else {
                     text2.setVisibility(View.INVISIBLE);
                     rGroup1.setVisibility(View.INVISIBLE);
                     /*btnOK.setVisibility(View.INVISIBLE);*/
                     imgPet.setVisibility(View.INVISIBLE);
+                    exitButton.setVisibility(View.INVISIBLE);
+                    clear.setVisibility(View.INVISIBLE);
+
                 }
             }
         });
@@ -116,7 +123,48 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });*/
-       
+
+    exitButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("정말로 종료 하시겠습니까?");
+            builder.setTitle("종료 알림창")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.setTitle("종료 알림창");
+            alert.show();
+            //단순종료시
+            //finish();
+        }
+
+    });
+
+
+    clear.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            text2.setVisibility(View.INVISIBLE);
+            rGroup1.setVisibility(View.INVISIBLE);
+            imgPet.setVisibility(View.INVISIBLE);
+            exitButton.setVisibility(View.INVISIBLE);
+            clear.setVisibility(View.INVISIBLE);
+            rGroup1.clearCheck();
+            sw.setChecked(false);
+        }
+    });
 
     }
 
